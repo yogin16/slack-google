91ecafe14bf40cd10886ca50ca7fd7f3c4117db9
Yogin Patel <ypatel@sprinklr.com>
2017-01-08 16:45:03 +0530

    add post listener

--- app.js
+++ app.js
@@ -5,6 +5,17 @@ app.get('/', function (req, res) {
   res.send('Hello World!')
 })
 
+app.post('/', function (req, res) {
+  res.send({
+    "text": "Hello World!",
+    "attachments": [
+        {
+            "text":"Example attachments"
+        }
+    ]
+})
+})
+
 var PORT = process.env.PORT || 3000;
 app.listen(PORT, function () {
   console.log('Example app listening on port 3000!')
