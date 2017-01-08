3186c52a4d5203915dfdf0dc5adc79ac4a47ef80
Yogin Patel <ypatel@sprinklr.com>
2017-01-08 16:20:38 +0530

    heroku port

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ app.get('/', function (req, res) {
   res.send('Hello World!')
 })
 
-app.listen(3000, function () {
+var PORT = process.env.PORT || 3000;
+app.listen(PORT, function () {
   console.log('Example app listening on port 3000!')
 })
