14cd6d7f739d4e480145c4f4ec7b965c05175587
Yogin Patel <ypatel@sprinklr.com>
2017-01-08 18:15:30 +0530

    add google api and npm modules

--- app.js
+++ app.js
@@ -1,22 +1,41 @@
-var express = require('express')
-var app = express()
+var express = require('express');
+var google = require('google');
+var _ = require('lodash');
+var app = express();
 
 app.get('/', function (req, res) {
   res.send('Hello World!')
 })
 
 app.post('/', function (req, res) {
-  res.send({
-    "text": "Hello World!",
-    "attachments": [
-        {
-            "text":"Example attachments"
+  execute(req, res)
+})
+
+var execute = function (req, res) {
+  var text = req.param('text');
+  var message = {
+  		response_type:"in_channel"
+	};
+  google(text, function (err, googleRes){
+    if (err) {
+      message.text = JSON.stringify(err);
+    }
+    else {
+      message.text = "<" + googleRes.url + "|" + googleRes.query + ">";
+      message.attachments = _.map(googleRes.links, function(link){
+        return {
+          color: "#36a64f",
+          title: link.title,
+          title_link: link.link,
+          text: link.description
         }
-    ]
-})
-})
+      }
+    }
+  });
+  res.json(message);
+}
 
 var PORT = process.env.PORT || 3000;
 app.listen(PORT, function () {
-  console.log('Example app listening on port 3000!')
+  console.log('Example app listening on port 3000!');
 })
